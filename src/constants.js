87b20c307853a7b85bf5fa9c8cb829c6c1de1462
Etienne BONDOT <etiennebondot@gmail.com>
2018-03-16 13:06:56 +0100

    Define COLORS

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+    ARTICLE: {
+        TITLE: '#212121',
+    },
+    BUTTON: {
+        FOCUSED: 'rgba(153, 153, 153, 0.2)',
+        PRESSED: 'rgba(153, 153, 153, 0.4)',
+        DISABLED: 'rgba(153, 153, 153, 0.2)',
+    },
+};
