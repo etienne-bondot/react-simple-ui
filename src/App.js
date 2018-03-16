283c3330ab52db412b21379e48e04959e7c542b4
Etienne BONDOT <etiennebondot@gmail.com>
2018-03-16 13:08:42 +0100

    Update content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+
+import { Navbar, NavbarTitle } from './components/navbar/Navbar';
+import Content from './containers/Content';
+import { Section, SectionTitle } from './components/section/Section';
+import { Article, ArticleContent, ArticleTitle } from './components/article/Article';
 import Button from './components/button/Button';
 import Icon from './components/icon/Icon';
 
@@ -6,16 +11,35 @@ class App extends Component {
     render() {
         return (
             <div>
-                <section>
-                    <h2>Buttons</h2>
-                    <div>
-                        <Button disabled>Button</Button>
-                    </div>
-                </section>
-                <section>
-                    <h2>Icons</h2>
-                    <Icon icon="coffee"/>
-                </section>
+                <Navbar>
+                    <NavbarTitle>React-UI</NavbarTitle>
+                </Navbar>
+                <Content>
+                    <Section>
+                        <SectionTitle>Buttons</SectionTitle>
+                        <Article>
+                            <ArticleTitle>Button types</ArticleTitle>
+                            <ArticleContent>
+                                <p>There are two standard types of buttons:</p>
+                                <ul>
+                                    <li>Raised button</li>
+                                    <li>Flat button</li>
+                                </ul>
+                                <Button>Button</Button>
+                                <Button disabled>Button</Button>
+                            </ArticleContent>
+                        </Article>
+                    </Section>
+                    <Section>
+                        <SectionTitle>Icons</SectionTitle>
+                        <Article>
+                            <ArticleTitle>Icons types</ArticleTitle>
+                            <ArticleContent>
+                                <Icon icon="coffee"/>
+                            </ArticleContent>
+                        </Article>
+                    </Section>
+                </Content>
             </div>
         );
     }
