1b1352a7d3adb271b6be3bce60f30e542474950f
Etienne BONDOT <etiennebondot@gmail.com>
2018-03-19 11:51:02 +0100

    Fix transparent baground

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,6 +6,7 @@ const headerStyle = {
     alignItems: 'center',
     height: 64,
     boxShadow: '0 2px 5px rgba(0,0,0,.26)',
+    backgroundColor: '#fff',
     left: 0,
     right: 0,
     top: 0,
@@ -20,9 +21,10 @@ const headerTitle = {
     fontWeight: 400,
 };
 
-const Navbar = ({title = '',}) =>
+const Navbar = ({title = '', children}) =>
     <header style={headerStyle}>
         <h1 style={headerTitle}>{title}</h1>
+        {children}
     </header>;
 
 Navbar.propTypes = {
