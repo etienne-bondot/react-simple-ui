ae8eaaedb12824966e6a4f04e55592aa25acf11d
Etienne BONDOT <etiennebondot@gmail.com>
2018-03-28 11:52:18 +0200

    Update content style according to new constants

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 import './Content.css';
+import { STYLES } from '../constants';
+
+const contentWrapperStyle = {
+    width: '100%',
+    height: '100%',
+    marginTop: STYLES.NAVBAR.HEIGHT,
+};
+
+const contentStyle = {
+    margin: 'auto',
+};
 
 const Content = ({children}) =>
-    <div className="ContentWrapper">
-        <div className="Content">{children}</div>
+    <div className="ContentWrapper" style={{...contentWrapperStyle}}>
+        <div className="Content" style={{...contentStyle}}>{children}</div>
     </div>;
 
 export default Content;
