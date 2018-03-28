edc2c92a33477acb633d2224e0968af2fab36320
Etienne BONDOT <etiennebondot@gmail.com>
2018-03-28 11:51:14 +0200

    Wrap all components with the ga-tracker

--- src/Routes.js
+++ src/Routes.js
@@ -5,13 +5,14 @@ import Content from './containers/Content';
 import ButtonView from './containers/guidelines/ButtonView';
 import IconView from './containers/guidelines/IconView';
 import InputView from './containers/guidelines/InputView';
+import withTracker from './withTracker';
 
 const Routes = () =>
     <Switch>
         <Route path="/" exact render={() => <Content>Hello</Content>}/>
-        <Route path="/guideline/button" exact component={ButtonView}/>
-        <Route path="/guideline/icon" exact component={IconView}/>
-        <Route path="/guideline/input" exact component={InputView}/>
+        <Route path="/guideline/button" exact component={withTracker(ButtonView)}/>
+        <Route path="/guideline/icon" exact component={withTracker(IconView)}/>
+        <Route path="/guideline/input" exact component={withTracker(InputView)}/>
     </Switch>;
 
 export default Routes;
