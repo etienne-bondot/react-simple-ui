9ac0d94107d6917eac4f71690542d4ac2bb8c921
Etienne BONDOT <etiennebondot@gmail.com>
2018-03-28 11:51:50 +0200

    Update constants structure: define COLORS and STYLES constants

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,15 @@
 export const COLORS = {
-    ARTICLE: {
-        TITLE: '#212121',
-    },
-    BUTTON: {
-        FOCUSED: 'rgba(153, 153, 153, 0.2)',
-        PRESSED: 'rgba(153, 153, 153, 0.4)',
-        DISABLED: 'rgba(153, 153, 153, 0.2)',
-    },
+    TITLE: '#212121',
+    FOCUSED: 'rgba(153, 153, 153, 0.2)',
+    PRESSED: 'rgba(153, 153, 153, 0.4)',
+    DISABLED: 'rgba(153, 153, 153, 0.2)',
+    WHITE: '#fff',
+    BLACK: '#000',
 };
+
+export const STYLES = {
+    NAVBAR: {
+        HEIGHT: 64,
+        BACKGROUND_COLOR: COLORS.WHITE,
+    }
+}
